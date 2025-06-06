5115230f8f4a492fa59fbafe8901675ea6391e08
AeongKim <jimmy1101@naver.com>
2025-06-07 07:34:05 +0900

    광고차단 추가

--- wwme.user.js
+++ wwme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         덥덥미 광고 차단
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @match        https://wwme.kr/*
 // @grant        none
 // ==/UserScript==
@@ -9,15 +9,17 @@
 (function () {
     'use strict';
 
-    // 1. 두 요소를 동시에 제거하는 함수 생성
+    // 광고 요소를 모두 제거하는 함수
     function removeAds() {
-        document.querySelectorAll('.xs\\:hidden.block, .lg\\:block.hidden').forEach(el => el.remove());
+        document.querySelectorAll(
+            '.xs\\:hidden.block, .lg\\:block.hidden, .lg\\:hidden.xs\\:block.hidden'
+        ).forEach(el => el.remove());
     }
 
-    // 2. 최초 실행
+    // 최초 실행
     removeAds();
 
-    // 3. 동적 요소 감지 로직
+    // 동적 요소 감지
     const observer = new MutationObserver(removeAds);
     observer.observe(document.body, { childList: true, subtree: true });
 })();
